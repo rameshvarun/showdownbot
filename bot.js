a48280d82f2bc54b9ba240a68d980d71b015fa03
Varun Ramesh <varun.ramesh@aol.com>
2014-10-26 15:30:57 -0700

    Logging + working on Battle room system

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 var sockjs = require('sockjs-client-ws');
 var request = require('request');
+var tools = require('./tools');
+
+// Setup Logging
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
 var client = sockjs.create("http://sim.smogon.com:8000/showdown");
-
-var tools = require('tools');
-
 var ACTION_PHP = "http://play.pokemonshowdown.com/~~showdown/action.php";
-
 var account = require("./account.json");
-
 
 var CHALLENGE_KEY_ID = null;
 var CHALLENGE = null;
@@ -19,6 +20,8 @@ function send(data, room) {
 		data = '|'+data;
 	}
 	client.write(data);
+
+	logger.trace(">> " + data);
 }
 
 function rename(name, password) {
@@ -38,13 +41,60 @@ function rename(name, password) {
 		if(data && data.curuser && data.curuser.loggedin) {
 			send("/trn " + account.username + ",0," + data.assertion);
 		} else {
-			console.log("Error logging in...");
+			// We couldn't log in for some reason
+			logger.fatal("Error logging in...");
+			process.exit();
 		}
 	});
 }
 
+var ROOMS = {};
+
 function recieve(data) {
-	// TODO(rameshvarun): Handle all the room redirection from client.js:733-801
+	logger.trace("<< " + data);
+
+	var roomid = '';
+	if (data.substr(0,1) === '>') {
+		var nlIndex = data.indexOf('\n');
+		if (nlIndex < 0) return;
+		roomid = tools.toRoomid(data.substr(1,nlIndex-1));
+		data = data.substr(nlIndex+1);
+	}
+	if (data.substr(0,6) === '|init|') {
+		if (!roomid) roomid = 'lobby';
+		var roomType = data.substr(6);
+		var roomTypeLFIndex = roomType.indexOf('\n');
+		if (roomTypeLFIndex >= 0) roomType = roomType.substr(0, roomTypeLFIndex);
+		roomType = tools.toId(roomType);
+
+		logger.info(roomid + " is being opened.");
+		addRoom(roomid, roomType, true);
+
+	} else if ((data+'|').substr(0,8) === '|expire|') {
+		var room = ROOMS[roomid];
+		logger.info(roomid + " has expired.");
+		if(room) {
+			room.expired = true;
+			if (room.updateUser) room.updateUser();
+		}
+		return;
+	} else if ((data+'|').substr(0,8) === '|deinit|' || (data+'|').substr(0,8) === '|noinit|') {
+		if (!roomid) roomid = 'lobby';
+
+		// expired rooms aren't closed when left
+		if (this.rooms[roomid] && this.rooms[roomid].expired) return;
+
+		logger.info(roomid + " has been closed.");
+		removeRoom(roomid);
+		return;
+	}
+	if(roomid) {
+		if(ROOMS[roomid]) {
+			ROOMS[roomid].recieve(data);
+		}
+		return;
+	}
+
 	var parts;
 	if(data.charAt(0) === '|') {
 		parts = data.substr(1).split('|');
@@ -53,23 +103,44 @@ function recieve(data) {
 	}
 
 	switch(parts[0]) {
+		// Challenge string
 		case 'challenge-string':
 		case 'challstr':
-			console.log("Recieved challenge string...");
+			logger.info("Recieved challenge string...");
 			CHALLENGE_KEY_ID = parseInt(parts[1], 10);
 			CHALLENGE = parts[2];
+
+			// Now try to rename to the given user
 			rename(account.username, account.password);
+			break;
+		// Server is telling use to update the user that we are currently logged in as
+		case 'updateuser':
+			// The update user command can actually come with a second command (after the newline)
+			var nlIndex = data.indexOf('\n');
+			if (nlIndex > 0) {
+				recieve(data.substr(nlIndex+1));
+				nlIndex = parts[3].indexOf('\n');
+				parts[3] = parts[3].substr(0, nlIndex);
+			}
+
+			var name = parts[1];
+			var named = !!+parts[2];
+
+			if(name == account.username) {
+				logger.info("Successfully logged in.");
+			}
+			break;
+		default:
+			logger.warn("Did not recognize command of type: " + parts[0]);
 			break;
 	}
 }
 
 client.on('connection', function() {
-	console.log('Connected to server.');
-	//rename(account.username, account.password);
+	logger.info('Connected to server.');
 });
 
 client.on('data', function(msg) {
-	console.log("Data from server:" + msg);
 	recieve(msg);
 });
 
--- tools.js
+++ tools.js
@@ -4,6 +4,15 @@ module.exports.safeJSON = function(data) {
 	return JSON.parse(data);
 }
 
+// Sanitizes a Room name
 module.exports.toRoomid = function(roomid) {
 	return roomid.replace(/[^a-zA-Z0-9-]+/g, '');
+}
+
+
+module.exports.toId = function(text) {
+	text = text || '';
+	if (typeof text === 'number') text = ''+text;
+	if (typeof text !== 'string') return toId(text && text.id);
+	return text.toLowerCase().replace(/[^a-z0-9]+/g, '');
 }
